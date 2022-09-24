c67dff2c2bb05d7a94d5953b8191d8ccfcae9383
Anna <eekolegov@mail.ru>
2022-09-24 20:53:38 +0300

    Zadacha10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,8 @@
+﻿void zadacha10()
+{
+    int number1=new Random().Next(100, 1000);
+    Console.WriteLine($"число:{number1}");
+    Console.WriteLine($"вторая цифра числа:{number1/10%10} ");
+}
+
+zadacha10();
