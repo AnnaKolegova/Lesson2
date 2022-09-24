914e072eca5a07045c3ec1bc78717b1d94c03ff7
Anna <eekolegov@mail.ru>
2022-09-24 21:23:22 +0300

    Zadacha15

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,17 @@
+﻿void zadacha15()
+{
+    int number1=new Random().Next(1,8);
+       Console.WriteLine($"число:{number1}");
+    if (number1<6)
+    {
+        Console.WriteLine("рабочий день");
+    }
+    else
+    {
+       
+        Console.WriteLine("выходной день");
+    }
+    
+}
+
+zadacha15();
