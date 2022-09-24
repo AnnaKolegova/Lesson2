58f4763bb265fb7e7f73d112ab29ba772fd7a8bb
Anna <eekolegov@mail.ru>
2022-09-24 21:18:40 +0300

    Zadacha13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,20 @@
+﻿void zadacha13()
+{
+    int number1=new Random().Next(0, 10000000);
+       Console.WriteLine($"число:{number1}");
+    if (number1<100)
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+    else
+    {
+        while (number1>999)
+        {
+            number1=number1/10;
+        }
+        Console.WriteLine($"третья цифра числа:{number1%10} ");
+    }
+    
+}
+
+zadacha13();
